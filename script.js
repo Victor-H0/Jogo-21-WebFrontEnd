914cc146017b553a853686e9477e3f9344fe8186
Victor Henrique <127879766+Victor-H0@users.noreply.github.com>
2024-05-17 20:23:25 -0300

    Deixei o Jogo Pronto até a Tela Inicial
    Ainda falta adicionar as funcionalidades dos botões, as telas de vitória e derrota, a revelação das cartas do PC....
    entre outras coisas. Mas o estado inicial do jogo está pronto

--- script.js
+++ script.js
@@ -2,33 +2,101 @@ const iniciarJogo = document.getElementById('iniciar-jogo');
 const modal = document.getElementById('modal');
 const Mesa = document.getElementById('mesa');
 const Soma = document.getElementById('soma');
+const Jogador = document.getElementById('jogador');
+const PC = document.getElementById('PC');
 
-var qtde1 = 4;
-var qtde2 = 4;
-var qtde3 = 4;
-var qtde4 = 4;
-var qtde5 = 4;
-var qtde6 = 4;
-var qtde7 = 4;
-var qtde8 = 4;
-var qtde9 = 4;
-var qtde10 = 4;
-var qtde11 = 4;
-var qtde12 = 4;
-var qtde13 = 4;
+var somatoria = 0;
+
+
+// A - 0 | 2 - 1 | 3 - 2 | 4 - 3 | 5 - 4 | 6 - 5 | 7 - 6 | 8 - 7 | 9 - 8 | 10 - 9 | J - 10 | Q - 11 | K - 12 |
+var qtde = [4,4,4,4,4,4,4,4,4,4,4,4,4];
+const valores = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+const converter = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
 
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-function criarValor() {
-    let index =  getRandomArbitrary(1,13);
+function indexDisponivel() {
+    let index =  getRandomArbitrary(0,13);
     
+    if(qtde[index] > 0){
+        qtde[index]--;
+        return index;
+    }
+    else{
+        return indexDisponivel();
+    }    
+}
+
+function addCarta(Numero){
+    let novaCarta = document.createElement("div");
+    novaCarta.classList.add("carta");
+
+    let contCarta = document.createElement("span");
+    contCarta.classList.add("valor");
+
+    let text = document.createTextNode(converter[Numero]);
+
+    contCarta.appendChild(text);
+    novaCarta.appendChild(contCarta);
+
+    return novaCarta;
+}
+
+function escurecerCartas(){
+    var darkCarta0 = document.getElementsByClassName("carta")[0];
+    var darkCarta1 = document.getElementsByClassName("carta")[1];
+    var darkNum0 = document.getElementsByClassName("valor")[0];
+    var darkNum1 = document.getElementsByClassName("valor")[1];
+
+    darkNum0.style.display = "none";
+    darkNum1.style.display = "none";
+
+    darkCarta0.style.backgroundColor = "red";
+    darkCarta1.style.backgroundColor = "red";
+}
+
+function calcularSoma(){
+    let qtdeA = 0; 
+    let parcela = 0;   
+
+    for(let i=0; i < Jogador.childElementCount; i++){
+
+        let valor = valores[converter.indexOf(Jogador.children[i].firstChild.innerHTML)];
+
+        if(valor == 11){
+            qtdeA++;
+        }
+
+        parcela += valor;
+    }
+
+    while(parcela > 21 && qtdeA > 0){
+        parcela -= 10;
+        qtdeA--;
+    }
+
+    somatoria = parcela;
 }
 
 iniciarJogo.addEventListener('click', () => {
     modal.style.display = 'none';
     Mesa.style.display = 'flex';
     Soma.style.display = 'block';
-    });
+
+    PC.appendChild(addCarta(indexDisponivel()));
+    PC.appendChild(addCarta(indexDisponivel()));
+
+    escurecerCartas();
+
+    Jogador.appendChild(addCarta(indexDisponivel()));
+    Jogador.appendChild(addCarta(indexDisponivel()));
+
+    calcularSoma();
+
+    soma.textContent = `Soma: ${somatoria}`;
+});
+
 
