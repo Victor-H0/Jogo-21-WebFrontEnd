88720fc712aa0afe2fd47412ede73966fb18631b
Victor Henrique <127879766+Victor-H0@users.noreply.github.com>
2024-05-19 23:39:23 -0300

    Fiz modificações na lógica do jogo
    Acho que a lógica já está pronta, deixei alguns comentários no código tentando explicar oq eu fiz.
    Na teoria o jogo todo ja está pronto, a não ser que vc queira adicionar as telas de vitória/derrota/empate.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const iniciarJogo = document.getElementById('iniciar-jogo');
 const modal = document.getElementById('modal');
 const Mesa = document.getElementById('mesa');
-const Soma = document.getElementById('soma');
+const TotalJog = document.getElementById('TotalJog');
+const TotalPC = document.getElementById('TotalPC');
 const Jogador = document.getElementById('jogador');
 const PC = document.getElementById('PC');
 const comprar = document.getElementById('comprar');
 const parar = document.getElementById('parar');
+const rotuloJogador = document.getElementById('nomeJogador');
+const rotuloPC = document.getElementById('nomePC');
 
 var somaPC = 0;
 var somaJogador = 0;
@@ -19,6 +22,12 @@ const converter = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", 
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
+
+
+  function esperar(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+ }
+  
 
 function indexDisponivel() {
     let index =  getRandomArbitrary(0,13);
@@ -47,26 +56,30 @@ function addCarta(Numero){
     return novaCarta;
 }
 
-function escurecerCartas(){
-    const darkCarta = PC.getElementsByClassName('carta');
-    const darkNum = PC.getElementsByClassName('valor');
 
-    for(let i = 0; i < darkCarta.length; i++){
-        darkCarta[i].style.background = "red";
-    }
+function escurecerCarta(){
 
-    for(let i = 0; i < darkNum.length; i++){
-        darkNum[i].style.display = "none";
-    }
+    //Vi umas regras do jogo e pelo visto só umas das cartas do PC fica virada pra baixo
+
+    const darkCarta = PC.getElementsByClassName('carta')[0];
+    const darkNum = PC.getElementsByClassName('valor')[0];
+
+    darkCarta.style.backgroundColor = "red";
+    darkNum.style.display = "none";
+
 }
 
-function calcularSomaJogador(){
+
+function calcularSoma(Player){
+
+    //Juntei as duas funções de somar em uma funcão só, com o parâmetro do jogador que vc quer calcular a soma
+
     let qtdeA = 0; 
     let parcela = 0;   
 
-    for(let i=0; i < Jogador.childElementCount; i++){
+    for(let i=0; i < Player.childElementCount; i++){
 
-        let valor = valores[converter.indexOf(Jogador.children[i].firstChild.innerHTML)];
+        let valor = valores[converter.indexOf(Player.children[i].firstChild.innerHTML)];
 
         if(valor == 11){
             qtdeA++;
@@ -80,116 +93,182 @@ function calcularSomaJogador(){
         qtdeA--;
     }
 
-    somaJogador = parcela;
+    return parcela;
 }
 
-function calcularSomaPC(){
-    let qtdeA = 0;
-    let parcela = 0;
 
-    for(let i=0; i < PC.childElementCount; i++) {
+async function verificarSoma(){
 
-        let valor = valores[converter.indexOf(PC.children[i].firstChild.innerHTML)];
+    await esperar(1000);
 
-        if(valor == 11){
-            qtdeA++;
-        }
 
-        parcela += valor;
+    //Caso o Jogagor ou o PC estourem o limite
+    if(somaJogador > 21 && somaPC > 21){
+        alert('Não houve vencedores!');
+        reiniciar();
+    }
+    else if(somaJogador > 21 && somaPC < 22){
+        alert('Você Perdeu!');
+        reiniciar();
+    }
+    else if(somaJogador < 22 && somaPC > 21){
+        alert('Parabéns, você venceu!');
+        reiniciar();
     }
 
-    somaPC = parcela;
-}
-
-function verificarSoma(){
-    if (somaJogador == 21) {
-        setTimeout(function() {
-            alert('Parabéns, você venceu!');
-            reiniciar();
-        }, 1000);
+    //Caso ninguém estoure
+    else if(somaJogador == somaPC){
+        alert('Não houve vencedores!');
+        reiniciar();
     }
-
-    if (somaPC == 21) {
-        setTimeout(function() {
-            alert('Perdeu!');
-            reiniciar();
-        }, 1000); 
+    else if(somaJogador > somaPC){
+        alert('Parabéns, você venceu!');
+        reiniciar();
     }
-
-    if (somaJogador > 21 && somaPC < 21) {
-        setTimeout(function() {
-            alert('Perdeu!');
-            reiniciar();
-        }, 1000); 
+    else if(somaJogador < somaPC){
+        alert('Perdeu!');
+        reiniciar();
     }
+    // if (somaJogador == 21) {
+    //     setTimeout(function() {
+    //         alert('Parabéns, você venceu!');
+    //         reiniciar();
+    //     }, 1000);
+    // }
 
-    if (somaJogador < 21 && somaPC > 21) {
-        setTimeout(function() {
-            alert('Parabéns, você venceu!');
-            reiniciar();
-        }, 1000);
-    }
+    // if (somaPC == 21) {
+    //     setTimeout(function() {
+    //         alert('Perdeu!');
+    //         reiniciar();
+    //     }, 1000); 
+    // }
 
-    if (somaJogador > 21 && somaPC > 21) {
-        setTimeout(function() {
-            alert('Não houve vencedores!');
-            reiniciar();
-        }, 1000); 
-    }
+    // if (somaJogador > 21 && somaPC < 21) {
+    //     setTimeout(function() {
+    //         alert('Perdeu!');
+    //         reiniciar();
+    //     }, 1000); 
+    // }
+
+    // if (somaJogador < 21 && somaPC > 21) {
+    //     setTimeout(function() {
+    //         alert('Parabéns, você venceu!');
+    //         reiniciar();
+    //     }, 1000);
+    // }
+
+    // if (somaJogador > 21 && somaPC > 21) {
+    //     setTimeout(function() {
+    //         alert('Não houve vencedores!');
+    //         reiniciar();
+    //     }, 1000); 
+    // }
 }
 
 function reiniciar() {
-    window.location.href = window.location.href;
+    //window.location.href = window.location.href;
     location.reload(true);
+}
+
+async function comprarPC(){
+    while(somaPC < 17){
+        await esperar(1500);
+        alert('O Dealer Irá Comprar');
+        PC.appendChild(addCarta(indexDisponivel()));
+        somaPC = calcularSoma(PC);
+        TotalPC.textContent = `Soma das Cartas do Dealer: ${somaPC}`;        
+    }
+    verificarSoma();
+}
+
+function parada(){
+
+    PC.getElementsByClassName('carta')[0].style.backgroundColor = "whitesmoke";
+    PC.getElementsByClassName('valor')[0].style.display = "block";
+    somaPC = calcularSoma(PC);
+    TotalPC.textContent = `Soma das Cartas do Dealer: ${somaPC}`;
+
+    comprarPC();
+
+    // verificarSoma();
 }
 
 iniciarJogo.addEventListener('click', () => {
     modal.style.display = 'none';
     Mesa.style.display = 'flex';
-    Soma.style.display = 'block';
+    TotalJog.style.display = 'block';
+    rotuloJogador.style.display = 'block';
+    rotuloPC.style.display = 'block';
 
     PC.appendChild(addCarta(indexDisponivel()));
     PC.appendChild(addCarta(indexDisponivel()));
 
-    calcularSomaPC();
-    escurecerCartas();
+    //somaPC = calcularSoma(PC);
+    escurecerCarta();
 
     Jogador.appendChild(addCarta(indexDisponivel()));
     Jogador.appendChild(addCarta(indexDisponivel()));
 
-    calcularSomaJogador();
+    somaJogador = calcularSoma(Jogador);
 
-    Soma.textContent = `Soma: ${somaJogador}`;
-    
-    verificarSoma();
+    TotalJog.textContent = `Soma das Suas Cartas: ${somaJogador}`;
+
+    if(somaJogador == 21){
+        comprar.disabled = true;
+    }    
+    //verificarSoma();
 });
 
 comprar.addEventListener('click', () => {
+
+    //Na regra do jogo somente o jogador compra carta no botão comprar. O PC só compra carta dps q o Jogador resolve parar e se tiver com menos de 17 na somaPC
+
     Jogador.appendChild(addCarta(indexDisponivel()));
+    somaJogador = calcularSoma(Jogador);
+    TotalJog.textContent = `Soma das Suas Cartas: ${somaJogador}`;
 
-    calcularSomaJogador();
-    Soma.textContent = `Soma: ${somaJogador}`;
+    if(somaJogador == 21){
+        comprar.disabled = true;
+        parar.disabled = true;
+        setTimeout(function() {
+            alert('Voce atingiu 21, não é possivel comprar mais cartas');
+            parada();
+        }, 1000);
+    }
+    else if(somaJogador > 21){
+        comprar.disabled = true;
+        parar.disabled = true;
+        setTimeout(function() {
+            alert('Voce ultrapassou 21, não é possivel comprar mais cartas');
+            parada();
+        }, 1000);
+    }
 
-    PC.appendChild(addCarta(indexDisponivel()));
+    // PC.appendChild(addCarta(indexDisponivel()));
 
-    calcularSomaPC();
-    escurecerCartas();
+    // somaPC = calcularSoma(PC);
+    // escurecerCartas();
 
-    verificarSoma();
+    // verificarSoma();
 });
 
 parar.addEventListener('click', () => {
-    if (somaJogador < 21 && somaPC < 21) {
-        if (somaJogador > somaPC) {
-            alert('Parabéns, você venceu!');
-            reiniciar();
-        }
+    parar.disabled = true;
+    comprar.disabled = true;
+    parada();
 
-        if (somaJogador < somaPC) {
-            alert('Perdeu!');
-            reiniciar();
-        }
-    }
+
+    // if (somaJogador < 21 && somaPC < 21) {
+    //     if (somaJogador > somaPC) {
+    //         alert('Parabéns, você venceu!');
+    //         reiniciar();
+    //     }
+
+    //     if (somaJogador < somaPC) {
+    //         alert('Perdeu!');
+    //         reiniciar();
+    //     }
+    // }
 });
 
 
